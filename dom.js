6cb0854146c934cb20e335ed7913786ff6dfca23
Nehaa-04 <nehabitodkar06@gmail.com>
2023-05-24 15:38:09 +0530

    add changes in dom.js using getElementsByClassName

--- dom.js
+++ dom.js
@@ -25,4 +25,28 @@ var header = document.getElementById('main-header');
 //headerTitle.style.borderBottom = 'solid 3px #000';
 header.style.borderBottom = 'solid 3px #000';
 
+//GetElementByClassName
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent="Hello 2";
+items[1].style.fontWeight='bold';
+items[1].style.backgroundColor='yellow';
+
+//items.style.backgroundColor='#f4f4f4'; 
+//not possible, we have to loopthrough it coz it is an array
+
+// for(var i=0; i<items.length; i++)
+// {
+//    items[i].style.backgroundColor='#f4f4f4'; 
+// }
+
+//Deliverable
+items[2].style.backgroundColor='green';
+
+for(var i=0; i<items.length; i++)
+{
+    items[i].style.fontWeight='bold';
+}
 
